5425ecee8fcfdfe999d1ac18fc615f117080cc21
Adam Jones <jones.adam@live.ca>
2018-10-21 16:43:51 -0600

    added untested stdDev

--- assign3/problem3.cpp
+++ assign3/problem3.cpp
@@ -1,10 +1,29 @@
 /*assignment3 problem3
+computes standard deviation of a number set without needing to keep track
+of previous specific set elements. All that matters is cumulative
+totals and the current data point to be used as 
+parameters in iterative comuputations
 */
 
 #include <iostream>
-using namespace std::cout;
+using std::cout;
+
+float data = 0;
+float numElements = 0; // num of elements in data set
+float stdDev = 0; // cumulative standard deviation
+float mean = 0; // cumulative mean
+
 
 int main() {
-	cout << "test\n";
+	cin << data;
+	while (data != 0) {
+		count++; // count incr only after data is validated
+		mean = mean + data / numElements
+		
+		stdDev = float sqrt( pow((data + mean), 2) / (numElements + 1))
+		
+		cin << data;
+	}
+	
 	return 0;
 }
