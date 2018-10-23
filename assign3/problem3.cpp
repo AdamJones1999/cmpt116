e71580668da4301a7a0f7357e0ad35ae8b44079e
Adam Jones <jones.adam@live.ca>
2018-10-23 01:09:48 -0600

    Fixed stdDev formula, mean formula, and added exceptions for when only 1 element in data set.

--- assign3/problem3.cpp
+++ assign3/problem3.cpp
@@ -5,27 +5,39 @@ totals and the current data point to be used as
 parameters in iterative comuputations
 */
 
+#include <cmath>
 #include <iostream>
-using std::cout;
+using namespace std;
 
-float data = 0;
-float numElements = 0; // num of elements in data set
-float stdDev = 0; // cumulative standard deviation
-float mean = 0; // cumulative mean
+double data = 0;
+double numElements = 0; // num of elements in data set
+double stdDev = 0; // cumulative standard deviation
+double mean = 0; // cumulative mean
 
 
 int main() {
-	cin << data;
+	cin >> data;
 	while (data != 0) {
-		count++; // count incr only after data is validated
-		mean = mean + data / numElements
+		numElements++; // count incr only after data is validated
+		mean = mean + (data - mean) / numElements;
+		if (numElements == 1) { /*avoids undefined behaviour in c++ program
+			(div by 0 in stdDev formula)*/
+		}
+		else {
+			stdDev = sqrt( ( pow(data, 2) - pow(data, 2) / numElements ) / (numElements - 1));
+		}
 		
-		stdDev = float sqrt( pow((data + mean), 2) / (numElements + 1))
-		
-		cin << data;
+		cout << mean << ", " << stdDev << "\n";
+		cin >> data;
 	}
-	
-	cout << "mean: " << mean << "standard deviation: " << stdDev << "\n";
+	/*even though stdDev is initialized to 0, 0 is not what the stdDev is, by the 
+	formula used in this program, stdDev when numElements = 1 is undefined */
+	if (numElements == 1) {
+		cout << "mean: " << mean << " standard deviation: " << "undefinned" << "\n";
+	}
+	else {
+		cout << "mean: " << mean << " standard deviation: " << stdDev << "\n";
+	}
 	
 	return 0;
 }
