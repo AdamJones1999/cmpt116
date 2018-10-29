8aaba9e2873a9d4956438d8c7bd7ea81426c32df
Adam Jones <jones.adam@live.ca>
2018-10-28 23:52:43 -0600

    problem1 with instructions, but no invalid input messages yet.

--- assign4/problem1.cpp
+++ assign4/problem1.cpp
@@ -1,5 +1,6 @@
 /*assignment 4 problem 1; due november 8 2018
 
+this essentially a horizontal reimann sum
 */
 #include <cmath>
 #include <iostream>
@@ -35,13 +36,22 @@ int main() {
 	a higher n means a more accurate calculation.\n";
 	cin >> n;
 	
-	for (z = 0; z >= -T; z += -T/n) {
-		//strip = (B / 2.0) * (1 - pow(2*x/L, 2)) * (1 - pow(z/T, 2));
-		
-		cout << z << ", ";
+	if (abs(x) > L / 2.0) {
+			cout << "invalid x coordinate. x must have smaller magnitude than half the \n\
+			hull's length (L/2) because the hull is symmetrical about the x-axis.";
+		}
+	else {
+		for (z = 0; z >= -T; z += -T/n) {
+		//formula for each individual strip that goes from waterline to hull bottom
+		strip = (B / 2.0) * (1 - pow(2*x/L, 2)) * (1 - pow(z/T, 2));
+
 		tot_area += strip;
-		
+		// for testing
+		cout << z << ", " << strip << "\n";
+		}
+		cout << "total area: " << tot_area << "\n";
 	}
+		
 	return 0;
 }
 
