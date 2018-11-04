c4bb05db04352409dd56235b01a5262e06ee790e
Adam Jones <jones.adam@live.ca>
2018-11-04 01:24:17 -0600

    added lecture examples of class and probs 1-3 on assign4

--- assign4/problem1.cpp
+++ assign4/problem1.cpp
@@ -8,9 +8,9 @@ using namespace std;
 
 int main() {
 	// beam, length, draft, point of interest
-	double B = 0, L = 0, T = 0, x = 0;
+	double beam = 0, length = 0, T = 0, x = 0;
 	// number of strips
-	double n = 0; 
+	double num_strips= 0; 
 	// z-axis step variable, in T/n increments
 	double z = 0;	
 	// cross sectional area summed
@@ -20,34 +20,34 @@ int main() {
 	
 	cout << "input B\n";
 	//"input beam, which is the widest point at normal waterline\n";
-	cin >> B;
+	cin >> beam;
 	cout << "input L\n";
 	//"input length, which is the length of the boat\n";
-	cin >> L;
+	cin >> length;
 	cout << "input T\n";
 	//"input draft, distance between waterline and bottom of the hull\n";
-	cin >> T;
+	cin >> T ;
 	cout << "input x\n";
 	//"input x, the point along the length of the hull at which \n\
 	the cross sectional area contained by the hull is calculated.\n";
-	cin >> x;
+	cin >> x ;
 	cout << "input n\n";
 	//"input n, the number of slices taken in the cross sectional area calculation, \n\
-	a higher n means a more accurate calculation.\n";
-	cin >> n;
+	a higher num_stripsmeans a more accurate calculation.\n";
+	cin >> num_strips;
 	
-	if (abs(x) > L / 2.0) {
+	if (abs(x) > length / 2.0) {
 			cout << "invalid x coordinate. x must have smaller magnitude than half the \n\
 			hull's length (L/2) because the hull is symmetrical about the x-axis.";
 		}
 	else {
-		for (z = 0; z >= -T; z += -T/n) {
+		for ( z = 0; z >= - T; z += - T / num_strips) {
 		//formula for each individual strip that goes from waterline to hull bottom
-		strip = (B / 2.0) * (1 - pow(2*x/L, 2)) * (1 - pow(z/T, 2));
+		strip = ( beam / 2.0) * (1 - pow(2 * x / L, 2)) * (1 - pow( z / T , 2));
 
-		tot_area += strip;
+		tot_area += strip * T/n;
 		// for testing
-		cout << z << ", " << strip << "\n";
+		cout << z << ", " << strip * T / num_strips<< "\n";
 		}
 		cout << "total area: " << tot_area << "\n";
 	}
